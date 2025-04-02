dd4e683d19f683cfe71e47c8e0bd2f68cfb66904
root <samy2091988@gmail.com>
2025-04-03 01:26:18 +0500

    chnage provider.tf file and rrsolve errors

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorageaccount"
+    storage_account_name = "tfstatestorageaccoun1"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
